e5a823ae64abe1db6675fca407751618da19c93f
q7654 <82663899+q7654@users.noreply.github.com>
2022-09-02 22:56:23 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
 let obj = JSON.parse($response.body);
 
 obj["acquire"]=true;
-obj["currentPkg"]["productId"] = "12345";
-let t = obj["changeablePkg"][0];
-t["productId"] = "90063345";
-t["productName"] = "DW";
-t["pkgAct"] = null;
+
+obj["changeablePkg"][0] = obj["currentPkg"];
+obj["currentPkg"] = obj["changeablePkg"][1];
 
 
 $done({

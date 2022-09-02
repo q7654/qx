522da590f9aa543cc0f555b86d6ae1d185e4888e
q7654 <82663899+q7654@users.noreply.github.com>
2022-09-02 22:42:24 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 let obj = JSON.parse($response.body);
 
 obj["acquire"]=true;
+let t = obj["changeablePkg"][0];
+t["productId"] = "90063345";
+t["productName"] = "DW";
 
 $done({
 	body: JSON.stringify(obj)
